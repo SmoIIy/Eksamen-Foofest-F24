b7d30aad02ad48ba1b4bac1cfbaa2d46b915a914
Seb <sebastian.schyberg@gmail.com>
2024-05-08 13:54:45 +0200

    man kan ikke se noget, men der er heller ingen fejl xdd

---
+++ src/app/Landingpage/page.jsx
@@ -0,0 +1,6 @@
+export default function Landingpage() {
+  <main>
+    <h1>test</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis, dolor! Quaerat nobis expedita tempora, tenetur et, voluptas dolores, molestias ipsa debitis facilis doloribus fuga animi vel. Veniam quas dolores hic?</p>
+  </main>;
+}
--- src/app/pages/Landingpage/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function Landingpage() {
-  return (
-    <main>
-      <h1>test</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae aut expedita a quis ut ratione fugit hic qui dolorem sed consequuntur iure perspiciatis doloribus sunt commodi, nam, quia fugiat delectus?</p>
-    </main>
-  );
-}
