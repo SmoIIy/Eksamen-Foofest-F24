a34081b3b43036d1c5af4d0e72e2fd3298012652
Seb <sebastian.schyberg@gmail.com>
2024-05-10 14:52:17 +0200

    revert changes

--- src/app/Landingpage/page.jsx
+++ src/app/Landingpage/page.jsx
@@ -5,39 +5,21 @@ const data = fetchData(serverEndport + "/bands");
 console.log("data is " + Array.from(data));
 
 export default function Landingpage() {
-    if (!Array.isArray(data)) {
-        return <p>Error: Data is not in the expected format.</p>;
-    }
-    //det er som om at dataen der bliver fetched ikke er et array???
-    //fordi det her bør egenligt virke
 
     return (
-        // <ul>
-        //      { {
-        //         data.map((data, index) => (
-        //             <li key={index}>
-        //                 <p>{data.name}</p>
-        //             </li>
-        //         ))
-        //     }
-        //     <li>
-        //         <p>
-        //             {data.name}
-        //         </p>
-        //     </li>}
-        // </ul>
-        <div>
-            {data.map((band, index) => (
-                <div key={index}>
-                    <p>Name: {band.name}</p>
-                    <p>Genre: {band.genre}</p>
-                    <p>Members: {band.members.join(", ")}</p>
-                    <p>Bio: {band.bio}</p>
-                    <img src={band.logo} alt={band.name + " logo"} />
-                    <p>Logo Credits: {band.logoCredits}</p>
-                    <p>Slug: {band.slug}</p>
-                </div>
-            ))}
-        </div>
+        <ul>
+             { {
+                data.map((data, index) => (
+                    <li key={index}>
+                        <p>{data.name}</p>
+                    </li>
+                ))
+            }
+            <li>
+                <p>
+                    {data.name}
+                </p>
+            </li>}
+        </ul>
     );
 }
