2f423ce6448f6d9530c54ef40ee8324643219c09
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-05-13 21:43:56 +0200

    En masse

--- src/app/modules/functions.js
+++ src/app/modules/functions.js
@@ -2,7 +2,7 @@ export async function fetchData(endpoint) {
     try {
         const response = await fetch(endpoint);
         const data = await response.json();
-        console.table(data);
+        //console.table(data);
         return data;
     } catch (error){
         console.error(error);
--- src/app/modules/settings.js
+++ src/app/modules/settings.js
@@ -2,8 +2,10 @@ export const serverEndpoint = "https://mgp-allstars-2009-server.glitch.me/";
 
 export const localEndpoint = "http://localhost:8080"
 
-export const endpoint = localEndpoint;
+export const endpoint = serverEndpoint;
 
 export const apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndtbmZrcml0dHd0cW1wam94aHZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU2MDU5NjIsImV4cCI6MjAzMTE4MTk2Mn0.fo2Ir3QxLUQbmb7fkeX8BZd6fFBVwh0VUK7091-Ddgo";
 
 export const databaseEndport = "https://wmnfkrittwtqmpjoxhvn.supabase.co/rest/v1/bookings"
+
+export const databaseTestEndport = "https://wmnfkrittwtqmpjoxhvn.supabase.co/rest/v1/bookingtesting"
