1ecd0da2749e006ef6b83620af3b34ca87946ee0
Seb <sebastian.schyberg@gmail.com>
2024-05-14 13:25:50 +0200

    delete

--- src/app/schedule/page.jsx
+++ src/app/schedule/page.jsx
@@ -6,35 +6,8 @@ export default async function Schedule() {
     const data = await fetchData(endpoint + "/schedule");
     return (
         <div>
-            {Object.keys(data).map((location) => (
-                <div key={location} className="mb-4">
-                    <h2>{location}</h2>
-                    <table className="border-collapse">
-                        <thead>
-                            <tr>
-                                <th className="border py-2 px-4">Day</th>
-                                <th className="border py-2 px-4">Start</th>
-                                <th className="border py-2 px-4">End</th>
-                                <th className="border py-2 px-4">Act</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {Object.keys(data[location]).map((day) => (
-                                <tr key={day} className="border-t">
-                                    <td className="border py-2 px-4">{day}</td>
-                                    {data[location][day].map((slot, index) => (
-                                        <React.Fragment key={index}>
-                                            <td className="border py-2 px-4">{slot.start}</td>
-                                            <td className="border py-2 px-4">{slot.end}</td>
-                                            <td className="border py-2 px-4">{slot.act}</td>
-                                        </React.Fragment>
-                                    ))}
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            ))}
+            <h1>test</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, aut ratione doloribus deserunt obcaecati beatae labore praesentium magnam quidem! Facere possimus fugit odio distinctio nemo eum autem debitis laudantium nostrum.</p>
         </div>
     );
 }
