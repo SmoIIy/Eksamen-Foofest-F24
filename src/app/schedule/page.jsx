7ce76d47857d4162c09cb8420173712dabd61778
Seb <sebastian.schyberg@gmail.com>
2024-05-15 13:25:26 +0200

    forfra :)

--- src/app/schedule/page.jsx
+++ src/app/schedule/page.jsx
@@ -1,54 +1,15 @@
-"use client";
-
-import React, { useState } from "react";
+import { useQuery, useMutation } from "@tanstack/react-query";
 import { fetchData } from "@/app/modules/functions";
 import { endpoint } from "@/app/modules/settings";
 
-const Schedule = () => {
-    const [selectedDay, setSelectedDay] = useState(null);
-    const [scheduleData, setScheduleData] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+console.log(typeof data);
 
-    const handleDayClick = async (day) => {
-        try {
-            setLoading(true);
-            const data = await fetchData(endpoint + `/schedule/${day}`);
-            setScheduleData(data);
-            setSelectedDay(day);
-        } catch (error) {
-            setError(error.message);
-        } finally {
-            setLoading(false);
-        }
-    };
-
+export default async function Landingpage() {
+    const data = await fetchData(endpoint + "/schedule");
     return (
         <div>
-            <h1>Test</h1>
-            <div>
-                <button onClick={() => handleDayClick("mon")}>Monday</button>
-                <button onClick={() => handleDayClick("tue")}>Tuesday</button>
-                <button onClick={() => handleDayClick("wed")}>Wednesday</button>
-                <button onClick={() => handleDayClick("thu")}>Thursday</button>
-                <button onClick={() => handleDayClick("fri")}>Friday</button>
-                <button onClick={() => handleDayClick("sat")}>Saturday</button>
-                <button onClick={() => handleDayClick("sun")}>Sunday</button>
-            </div>
-            {loading && <p>Loading...</p>}
-            {error && <p>Error: {error}</p>}
-            {scheduleData && (
-                <div>
-                    <h2>Schedule for {selectedDay}</h2>
-                    <ul>
-                        {scheduleData.map((item) => (
-                            <li key={item.id}>{item.title}</li>
-                        ))}
-                    </ul>
-                </div>
-            )}
+            <h1>Schedule</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quidem magni corrupti autem dolores at nesciunt enim quasi culpa fugit dolorem error doloribus ad. Quae nam dolore molestias cumque soluta?</p>
         </div>
     );
-};
-
-export default Schedule;
+}
