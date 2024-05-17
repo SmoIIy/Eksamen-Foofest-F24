10d7df4cf835bb47fef70a7b4dfcccd621115dec
Seb <sebastian.schyberg@gmail.com>
2024-05-17 18:16:40 +0200

    tror ikke den kan commit, sidder i et fly .-.

---
+++ src/app/components/BandCard.jsx
@@ -0,0 +1,16 @@
+import {useQuery, useMutation } from "@tanstack/react-query";
+import { fetchData } from "@/app/modules/functions";
+import { endpoint } from "@/app/modules/settings";
+
+export default async function BandCard ({name, img, alt}){
+    return (
+  <div>
+    <a href="">
+    <h2>{name}</h2>
+    <img src={img} alt={alt} />
+    </a>
+  </div>
+    )
+    
+
+}
--- src/app/lineup/page.jsx
+++ src/app/lineup/page.jsx
@@ -1,6 +1,7 @@
 import {useQuery, useMutation } from "@tanstack/react-query";
 import { fetchData } from "@/app/modules/functions";
 import { endpoint } from "@/app/modules/settings";
+import BandCard from "@/app/components/BandCard";
 
 
 
@@ -12,20 +13,15 @@ export default async function Lineup() {
 
     const data = await fetchData(endpoint + "/bands");
     return (
+<ul>
+     {Object.keys(data).map(key =>
+                <li key={key}>
+                <BandCard name={data[key].name} img={data[key].logo}/>
+                </li>
+            )} 
+</ul>
 
-        // <ul>
-        //      {Object.keys(data).map(key =>
-        //         <li key={key}>
-        //             <p className="font-bold">{data[key].name}</p>
-        //             <p>{data[key].genre}</p>
-        //         </li>
-        //     )} 
-        //     </ul>
-        <div>
-            <h1>Lineup</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt excepturi labore error similique vel omnis? Veritatis quae nobis laudantium, architecto voluptatibus rerum iure omnis ipsum eos? Accusamus, optio quo.
-            </p>
-        </div>
 
-    );
-}
+)
+    }
+
