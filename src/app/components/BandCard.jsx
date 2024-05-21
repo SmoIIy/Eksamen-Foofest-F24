6ce33b33650a2ab5bd8ba71df592807ae2efd564
Seb <sebastian.schyberg@gmail.com>
2024-05-21 11:55:33 +0200

    Vi ødelagde noget der virkede og så fixede vi det på en dum måde

--- src/app/components/BandCard.jsx
+++ src/app/components/BandCard.jsx
@@ -1,13 +1,23 @@
 import {useQuery, useMutation } from "@tanstack/react-query";
 import { fetchData } from "@/app/modules/functions";
 import { endpoint } from "@/app/modules/settings";
+import Image from "next/image";
 
 export default async function BandCard ({name, img, alt}){
     return (
   <div>
     <a href="">
     <h2>{name}</h2>
-    <img src={img} alt={alt} />
+    {/* <img src={`@/app/assets/bands/${img}`} alt={alt} /> */}
+   
+   <Image
+      src="https://source.unsplash.com/random/720x480?random=35372"
+      width={500}
+      height={500}
+      alt="test testensen"
+   />
+    
+ 
     </a>
   </div>
     )
