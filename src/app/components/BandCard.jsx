6c7646557472a17af1723bfa7ae7f04cbbbae6f4
Seb <sebastian.schyberg@gmail.com>
2024-05-21 13:46:59 +0200

    smoll styling

--- src/app/components/BandCard.jsx
+++ src/app/components/BandCard.jsx
@@ -3,24 +3,21 @@ import { fetchData } from "@/app/modules/functions";
 import { endpoint } from "@/app/modules/settings";
 import Image from "next/image";
 
-export default async function BandCard ({name, img, alt}){
-    return (
-  <div>
-    <a href="">
-    <h2>{name}</h2>
-    {/* <img src={`@/app/assets/bands/${img}`} alt={alt} /> */}
-   
-   <Image
-      src="https://source.unsplash.com/random/720x480?random=35372"
-      width={500}
-      height={500}
-      alt="test testensen"
-   />
-    
- 
-    </a>
-  </div>
-    )
-    
-//:D
+export default function BandCard({ name, img, alt }) {
+  return (
+    <div className="p-4 rounded-md shadow-md mb-4">
+      <a href="" className="block">
+        <h2 className="text-xl font-semibold mb-2">{name}</h2>
+        <div className="aspect-w-16 aspect-h-9">
+          <Image
+            src="https://source.unsplash.com/random/720x480?random=35372"
+            width={500}
+            height={500}
+            alt="test testensen"
+        />
+        </div>
+      </a>
+    </div>
+  );
 }
+
