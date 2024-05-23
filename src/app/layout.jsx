e18bf1cf82f9f1fa4d8056623a7b6380132c4c36
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-05-24 01:27:19 +0200

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,39 +3,40 @@ import "./globals.css";
 import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
-const poppins = Poppins({ 
-  subsets: ["latin"], 
-  weight: ['400', '700'] 
+const poppins = Poppins({
+	subsets: ["latin"],
+	weight: ["400", "700"],
 });
 
 export const metadata = {
-  title: "Foo Fest festival 2024",
-  description: "Foo Fest Metal festival 2024, the biggest festival with the biggest names of all time.",
+	title: "Foo Fest festival 2024",
+	description:
+		"Foo Fest Metal festival 2024, the biggest festival with the biggest names of all time.",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en" className="h-full">
-      <body className={`flex flex-col min-h-screen h-full ${poppins.className}`}>
-        <div className="flex-grow flex flex-col">
-          <header className="bg-black-blue text-white py-4 text-center">
-            <nav className="">
-              <Link href="/">Home</Link>
-              <Link href="/booking">Booking</Link>
-              <Link href="/schedule">Schedule</Link>
-              <Link href="/lineup">Lineup</Link>
-            </nav>
-          </header>
-          
-          <main className="flex-grow">
-            {children}
-          </main>
-        </div>
-        
-        <footer className="bg-black-blue text-white py-4 text-center">
-          <p>FooFest 2024</p>
-        </footer>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" className="h-full">
+			<body
+				className={`flex flex-col min-h-screen h-full ${poppins.className}`}
+			>
+				<div className="flex-grow flex flex-col">
+					<header className="bg-black-blue text-white py-4 text-center">
+						<nav className="flex justify-around max-w-xl m-auto">
+							<Link href="/">Home</Link>
+							<Link href="/booking">Booking</Link>
+							<Link href="/schedule">Schedule</Link>
+							<Link href="/lineup">Lineup</Link>
+						</nav>
+					</header>
+
+					<main className="flex-grow">{children}</main>
+				</div>
+
+				<footer className="bg-black-blue text-white py-4 text-center">
+					<p>FooFest 2024</p>
+				</footer>
+			</body>
+		</html>
+	);
 }
