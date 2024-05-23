f432b2a67adf64c74e51d6fb86d2072ca3f648b0
Seb <sebastian.schyberg@gmail.com>
2024-05-23 15:26:47 +0200

    hmm <Image> er en kælling

--- src/app/Landingpage/page.jsx
+++ src/app/Landingpage/page.jsx
@@ -1,15 +1,25 @@
 import { useQuery, useMutation } from "@tanstack/react-query";
 import { fetchData } from "@/app/modules/functions";
 import { endpoint } from "@/app/modules/settings";
+import Image from "next/image";
+import logoPic from "@/app/assets/logos/logo-full-color.png"
 
 console.log(typeof data);
 
 export default async function Landingpage() {
 	const data = await fetchData(endpoint + "/bands");
 	return (
-		<div>
-			<h1>Hej</h1>
-			<p>dinmor</p>
-		</div>
+<div >
+	<p>COPENHAGEN, REFSHALEØEN</p>
+	<Image
+	src={logoPic}
+	width={500}
+	height={500}
+	alt="FooFest logo"
+	>
+	
+	</Image>
+	<p>12 AUGUST 2024</p>
+</div>
 	);
 }
