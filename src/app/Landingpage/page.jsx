574dcb2f1148bd65319dd805b55dc2224750add3
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-05-24 15:42:35 +0200

    yehawmf

--- src/app/Landingpage/page.jsx
+++ src/app/Landingpage/page.jsx
@@ -2,16 +2,32 @@ import { useQuery, useMutation } from "@tanstack/react-query";
 import { fetchData } from "@/app/modules/functions";
 import { endpoint } from "@/app/modules/settings";
 import Image from "next/image";
-import logoPic from "@/app/assets/logos/logo-full-color.png";
+import logoPic from "@/app/assets/logos/full-black.svg";
 import background from "@/app/assets/images/bg.png";
-
-console.log(typeof data);
+import Heroimage from "../components/Heroimage";
 
 export default async function Landingpage() {
 	const data = await fetchData(endpoint + "/bands");
 	return (
-		<div className="relative h-full">
-			<Image src={background} alt="koncert"></Image>
+		<div className="grid place-items-center grid-cols-1">
+			<Image
+				className="col-start-1 row-start-1"
+				src={background}
+				alt="koncert"
+			></Image>
+			<section className="col-start-1 row-start-1 flex flex-col items-center">
+				<h1 className="sr-only">FooFest 2024</h1>
+				<h2 className="uppercase font-bold text-dark-purple">
+					Copenhagen, Refshaløen
+				</h2>
+				<Image
+					className=""
+					src={logoPic}
+					alt="logo"
+					height={200}
+				></Image>
+				<p className="uppercase font-semibold">12 August 2024</p>
+			</section>
 		</div>
 	);
 }
---
+++ src/app/components/Heroimage.jsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function Heroimage({ bg }) {
+	return <Image className="" src={bg} alt="koncert"></Image>;
+}
