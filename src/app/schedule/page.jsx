b0723f66bb6013131f1c7214d57d7665af3b0063
Seb <sebastian.schyberg@gmail.com>
2024-05-24 16:37:51 +0200

    style

--- src/app/schedule/page.jsx
+++ src/app/schedule/page.jsx
@@ -41,40 +41,58 @@ function Schedule() {
 		queryFn: () => fetchData(endpoint + "/bands"),
 	});
 
-	if (scheduleLoading || bandsLoading) return <div>Loading...</div>;
-	if (scheduleError || bandsError) return <div>Error loading data</div>;
+	if (scheduleLoading || bandsLoading)
+		return <div className="text-center text-white">Loading...</div>;
+	if (scheduleError || bandsError)
+		return (
+			<div className="text-center text-red-500">Error loading data</div>
+		);
 
 	const scenes = ["Midgard", "Vanaheim", "Jotunheim"];
 
 	return (
-		<div>
-			<div>
+		<div className="text-white m-4">
+			<div className="flex justify-center mb-6 space-x-4">
 				{scenes.map((scene) => (
-					<button key={scene} onClick={() => setSelectedScene(scene)}>
+					<button
+						key={scene}
+						onClick={() => setSelectedScene(scene)}
+						className={`px-4 py-2 rounded ${selectedScene === scene ? "bg-blue-500" : "bg-gray-700 hover:bg-gray-600"}`}
+					>
 						{scene}
 					</button>
 				))}
 			</div>
 			<div>
-				<h1>{selectedScene} Schedule</h1>
+				<h1 className="text-center text-3xl font-bold mb-4">
+					{selectedScene} Schedule
+				</h1>
 				{Object.entries(scheduleData[selectedScene]).map(
-					([day, events, slug]) => (
-						<div key={day}>
-							<h3>{day}</h3>
-							<ul>
+					([day, events]) => (
+						<div key={day} className="mb-6">
+							<h3 className="font-bold text-2xl mb-2">{day}</h3>
+							<ul className="space-y-4">
 								{events.map((event, index) => (
-									<li key={index}>
-										<div>
+									<li
+										key={index}
+										className="p-4 bg-gray-800 rounded-lg shadow"
+									>
+										<div className="text-lg mb-2">
 											{event.start} - {event.end}
 										</div>
-										{/* ---------- det er det her der driller ------ */}
 										<Link
 											href={`/lineup/${event.act.replace(/\s+/g, "-").toLowerCase()}`}
+											className="text-2xl font-bold text-blue-400 hover:text-blue-500 hover:underline"
 										>
 											{event.act}
 										</Link>
-										{/* ---------- det er det her der driller ------ */}
-										{event.cancelled ? "(Cancelled)" : ""}
+										{event.cancelled ? (
+											<span className="text-red-500 ml-2">
+												(Cancelled)
+											</span>
+										) : (
+											""
+										)}
 									</li>
 								))}
 							</ul>
