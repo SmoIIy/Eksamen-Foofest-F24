d5d384b61d8f5879c3345c054dd68bbfed3f4b2c
Seb <sebastian.schyberg@gmail.com>
2024-05-27 22:59:35 +0200

    louis vuitton væg

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,15 @@ module.exports = {
 		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
+		backgroundSize: {
+			"50%": "100%",
+		},
 		extend: {
 			backgroundImage: {
 				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
 				"gradient-conic":
 					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-				"logo-pattern": "url('/src/app/assets/logos/logowall.svg')",
+				"logo-pattern": "url('/logowall.svg')",
 			},
 			colors: {
 				"black-blue": "#020c1c",
