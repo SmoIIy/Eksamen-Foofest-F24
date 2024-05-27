5ac459ff36f784efdb3cedf34cf615d4de1a4e3d
Seb <sebastian.schyberg@gmail.com>
2024-05-27 13:08:26 +0200

    små dumme ting

--- src/app/schedule/page.jsx
+++ src/app/schedule/page.jsx
@@ -8,6 +8,8 @@ import {
 import { fetchData } from "@/app/modules/functions";
 import { endpoint } from "@/app/modules/settings";
 import Link from "next/link";
+import logoWall from "@/app/assets/logos/logowall.svg";
+import Image from "next/image";
 
 const queryClient = new QueryClient();
 
@@ -63,42 +65,47 @@ function Schedule() {
 					</button>
 				))}
 			</div>
-			<div>
-				<h1 className="text-center text-3xl font-bold mb-4">
-					{selectedScene} Schedule
-				</h1>
-				{Object.entries(scheduleData[selectedScene]).map(
-					([day, events]) => (
-						<div key={day} className="mb-6">
-							<h3 className="font-bold text-2xl mb-2">{day}</h3>
-							<ul className="space-y-4">
-								{events.map((event, index) => (
-									<li
-										key={index}
-										className="p-4 bg-dark-blue rounded-lg shadow max-w-screen-lg"
-									>
-										<div className="text-lg mb-2">
-											{event.start} - {event.end}
-										</div>
-										<Link
-											href={`/lineup/${event.act.replace(/\s+/g, "-").toLowerCase()}`}
-											className="text-2xl font-bold text-white hover:text-light-purple"
+			<h1 className="text-center text-3xl font-bold mb-4">
+				{selectedScene} Schedule
+			</h1>
+			<div className="grid md:grid-cols-2">
+				<div>
+					{Object.entries(scheduleData[selectedScene]).map(
+						([day, events]) => (
+							<div key={day} className="mb-6">
+								<h3 className="font-bold text-2xl mb-2">
+									{day}
+								</h3>
+								<ul className="space-y-4">
+									{events.map((event, index) => (
+										<li
+											key={index}
+											className="p-4 bg-dark-blue rounded-lg shadow max-w-screen-lg"
 										>
-											{event.act}
-										</Link>
-										{event.cancelled ? (
-											<span className="text-red-500 ml-2">
-												(Cancelled)
-											</span>
-										) : (
-											""
-										)}
-									</li>
-								))}
-							</ul>
-						</div>
-					),
-				)}
+											<div className="text-lg mb-2">
+												{event.start} - {event.end}
+											</div>
+											<Link
+												href={`/lineup/${event.act.replace(/\s+/g, "-").toLowerCase()}`}
+												className="text-2xl font-bold text-white hover:text-light-purple"
+											>
+												{event.act}
+											</Link>
+											{event.cancelled ? (
+												<span className="text-red-500 ml-2">
+													(Cancelled)
+												</span>
+											) : (
+												""
+											)}
+										</li>
+									))}
+								</ul>
+							</div>
+						),
+					)}
+				</div>
+				<div className=""></div>
 			</div>
 		</div>
 	);
